4bcb61d0cfc1d16990a11f82777c321b7262dd02
Branko Tomic <branko@spicefactory.co>
2019-12-03 11:12:49 +0100

    Fix spacing issue between carrier expansion panels on quote group page

--- src/components/QuoteGroup.tsx
+++ src/components/QuoteGroup.tsx
@@ -177,141 +177,147 @@ const QuoteGroup: React.FC<Props> = ({ id }) => {
           ))}
         </Grid>
       </Box>
-      {quotesByCarrier.map(([carrierId, quotes]) => {
-        // need to find all of the quoteDetail items across provided quotes
-        const quoteDetailItemsMerged = flow(
-          map(get('quoteDetails')),
-          flatten,
-          uniqWith(
-            (arrVal: QuoteDetail, othVal: QuoteDetail) =>
-              arrVal.Description === othVal.Description &&
-              arrVal.CostUnit === othVal.CostUnit &&
-              arrVal.Currency === othVal.Currency,
-          ),
-        )(quotes) as QuoteDetail[];
+      <Box mb={6}>
+        {quotesByCarrier.map(([carrierId, quotes]) => {
+          // need to find all of the quoteDetail items across provided quotes
+          const quoteDetailItemsMerged = flow(
+            map(get('quoteDetails')),
+            flatten,
+            uniqWith(
+              (arrVal: QuoteDetail, othVal: QuoteDetail) =>
+                arrVal.Description === othVal.Description &&
+                arrVal.CostUnit === othVal.CostUnit &&
+                arrVal.Currency === othVal.Currency,
+            ),
+          )(quotes) as QuoteDetail[];
 
-        const handlePanelClick = () => {
-          setSelectedPanel(carrierId);
-          window.scrollTo(0, 150);
-        };
+          const handlePanelClick = () => {
+            setSelectedPanel(carrierId);
+            window.scrollTo(0, 150);
+          };
 
-        return (
-          <Box id={carrierId} mb={5}>
-            <ExpansionPanel TransitionProps={{ unmountOnExit: true }} expanded={selectedPanel == carrierId}>
-              <ExpansionPanelSummary
-                aria-controls="panel1c-content"
-                expandIcon={<ExpandMoreIcon />}
-                onClick={handlePanelClick}
-              >
-                <Typography variant="h4">{carrierId}</Typography>
-              </ExpansionPanelSummary>
+          return (
+            <Box id={carrierId} mb={1}>
+              <ExpansionPanel TransitionProps={{ unmountOnExit: true }} expanded={selectedPanel == carrierId}>
+                <ExpansionPanelSummary
+                  aria-controls="panel1c-content"
+                  expandIcon={<ExpandMoreIcon />}
+                  onClick={handlePanelClick}
+                >
+                  <Typography variant="h4">{carrierId}</Typography>
+                </ExpansionPanelSummary>
 
-              <ExpansionPanelDetails className={classes.tableScroll}>
-                <Grid container>
-                  <Grid item xs={12}>
-                    <Paper>
-                      <Table size="small" aria-label="a dense table">
-                        <TableHead>
-                          <TableRow>
-                            <TableCell />
-                            {quotes.map((quote, index) => (
-                              <Fragment key={index}>
-                                <TableCell className={classes.currencyCell}>Currency</TableCell>
-                                <TableCell align="right">Cost Value</TableCell>
-                                <TableCell>Cost Unit</TableCell>
-                              </Fragment>
-                            ))}
-                          </TableRow>
-                        </TableHead>
-                        <TableBody>
-                          {quoteDetailItemsMerged
-                            .filter(quoteDetail =>
-                              quoteDetailFilterList.includes(quoteDetail.Description.toLowerCase()),
-                            )
-                            .map((quoteDetail, i) => (
-                              <TableRow key={i}>
-                                <TableCell component="th" scope="row">
-                                  {quoteDetail.Description}
-                                </TableCell>
-                                {quotes.map((quote: any) => {
-                                  const matchingQuoteDetail = quote.quoteDetails.filter(
-                                    (item: QuoteDetail) =>
-                                      quoteDetail.Description === item.Description &&
-                                      quoteDetail.CostUnit === item.CostUnit &&
-                                      quoteDetail.Currency === item.Currency,
-                                  );
+                <ExpansionPanelDetails className={classes.tableScroll}>
+                  <Grid container>
+                    <Grid item xs={12}>
+                      <Paper>
+                        <Table size="small" aria-label="a dense table">
+                          <TableHead>
+                            <TableRow>
+                              <TableCell />
+                              {quotes.map((quote, index) => (
+                                <Fragment key={index}>
+                                  <TableCell className={classes.currencyCell}>Currency</TableCell>
+                                  <TableCell align="right">Cost Value</TableCell>
+                                  <TableCell>Cost Unit</TableCell>
+                                </Fragment>
+                              ))}
+                            </TableRow>
+                          </TableHead>
+                          <TableBody>
+                            {quoteDetailItemsMerged
+                              .filter(quoteDetail =>
+                                quoteDetailFilterList.includes(quoteDetail.Description.toLowerCase()),
+                              )
+                              .map((quoteDetail, i) => (
+                                <TableRow key={i}>
+                                  <TableCell component="th" scope="row">
+                                    {quoteDetail.Description}
+                                  </TableCell>
+                                  {quotes.map((quote: any) => {
+                                    const matchingQuoteDetail = quote.quoteDetails.filter(
+                                      (item: QuoteDetail) =>
+                                        quoteDetail.Description === item.Description &&
+                                        quoteDetail.CostUnit === item.CostUnit &&
+                                        quoteDetail.Currency === item.Currency,
+                                    );
 
-                                  if (matchingQuoteDetail.length == 0) {
-                                    return <TableCell colSpan={3} />;
-                                  } else {
-                                    return matchingQuoteDetail.map((quoteDetailInstance: QuoteDetail, i: number) => (
-                                      <Fragment key={i}>
-                                        <TableCell className={classes.currencyCell}>
-                                          {quoteDetailInstance.Currency !== 'incl.' ? quoteDetailInstance.Currency : ''}
-                                        </TableCell>
-                                        <TableCell align="right">{quoteDetailInstance.CostValue}</TableCell>
-                                        <TableCell>
-                                          {quoteDetailInstance.Currency === 'incl.' ? quoteDetailInstance.Currency : ''}{' '}
-                                          {quoteDetailInstance.CostUnit}
-                                        </TableCell>
-                                      </Fragment>
-                                    ));
-                                  }
-                                })}
-                              </TableRow>
-                            ))}
+                                    if (matchingQuoteDetail.length == 0) {
+                                      return <TableCell colSpan={3} />;
+                                    } else {
+                                      return matchingQuoteDetail.map((quoteDetailInstance: QuoteDetail, i: number) => (
+                                        <Fragment key={i}>
+                                          <TableCell className={classes.currencyCell}>
+                                            {quoteDetailInstance.Currency !== 'incl.'
+                                              ? quoteDetailInstance.Currency
+                                              : ''}
+                                          </TableCell>
+                                          <TableCell align="right">{quoteDetailInstance.CostValue}</TableCell>
+                                          <TableCell>
+                                            {quoteDetailInstance.Currency === 'incl.'
+                                              ? quoteDetailInstance.Currency
+                                              : ''}{' '}
+                                            {quoteDetailInstance.CostUnit}
+                                          </TableCell>
+                                        </Fragment>
+                                      ));
+                                    }
+                                  })}
+                                </TableRow>
+                              ))}
 
-                          <TableRow>
-                            <TableCell component="th" scope="row">
-                              Service Details
-                            </TableCell>
-                            {quotes.map((quote: any) => (
-                              <Fragment>
-                                <TableCell key={quote.QuoteNumber} colSpan={3} align="center">
-                                  {quote.serviceDetails[0].Frequency} {quote.serviceDetails[0].Routing}{' '}
-                                  {quote.serviceDetails[0].TransitTime} days
-                                </TableCell>
-                              </Fragment>
-                            ))}
-                          </TableRow>
+                            <TableRow>
+                              <TableCell component="th" scope="row">
+                                Service Details
+                              </TableCell>
+                              {quotes.map((quote: any) => (
+                                <Fragment>
+                                  <TableCell key={quote.QuoteNumber} colSpan={3} align="center">
+                                    {quote.serviceDetails[0].Frequency} {quote.serviceDetails[0].Routing}{' '}
+                                    {quote.serviceDetails[0].TransitTime} days
+                                  </TableCell>
+                                </Fragment>
+                              ))}
+                            </TableRow>
 
-                          <TableRow>
-                            <TableCell component="th" scope="row">
-                              Quote Validity
-                            </TableCell>
-                            {quotes.map((quote, index) => (
-                              <Fragment key={index}>
-                                <TableCell colSpan={3} align="center">
-                                  {formatDate(quote.validityPeriod.from, 'd. MMMM')} –{' '}
-                                  {formatDate(quote.validityPeriod.to, 'd. MMMM')}
-                                </TableCell>
-                              </Fragment>
-                            ))}
-                          </TableRow>
-                        </TableBody>
-                        <TableFooter>
-                          <TableRow>
-                            <TableCell className={classes.noBorder} />
+                            <TableRow>
+                              <TableCell component="th" scope="row">
+                                Quote Validity
+                              </TableCell>
+                              {quotes.map((quote, index) => (
+                                <Fragment key={index}>
+                                  <TableCell colSpan={3} align="center">
+                                    {formatDate(quote.validityPeriod.from, 'd. MMMM')} –{' '}
+                                    {formatDate(quote.validityPeriod.to, 'd. MMMM')}
+                                  </TableCell>
+                                </Fragment>
+                              ))}
+                            </TableRow>
+                          </TableBody>
+                          <TableFooter>
+                            <TableRow>
+                              <TableCell className={classes.noBorder} />
 
-                            {quotes.map(quote => (
-                              <Fragment key={quote.id}>
-                                <TableCell className={classes.noBorder} />
-                                <TableCell className={classes.buttonContainer} colSpan={2}>
-                                  <QuoteItemActionButtons quote={quote} />
-                                </TableCell>
-                              </Fragment>
-                            ))}
-                          </TableRow>
-                        </TableFooter>
-                      </Table>
-                    </Paper>
+                              {quotes.map(quote => (
+                                <Fragment key={quote.id}>
+                                  <TableCell className={classes.noBorder} />
+                                  <TableCell className={classes.buttonContainer} colSpan={2}>
+                                    <QuoteItemActionButtons quote={quote} />
+                                  </TableCell>
+                                </Fragment>
+                              ))}
+                            </TableRow>
+                          </TableFooter>
+                        </Table>
+                      </Paper>
+                    </Grid>
                   </Grid>
-                </Grid>
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-          </Box>
-        );
-      })}
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            </Box>
+          );
+        })}
+      </Box>
     </Container>
   );
 };
