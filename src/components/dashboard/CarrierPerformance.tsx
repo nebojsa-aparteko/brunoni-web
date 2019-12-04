e5acb2e1e481380f3f1f66661226dd8736bb92f3
Jovan Erčić <jovan@spicefactory.co>
2019-12-04 11:20:54 +0100

    Fix carrier colors

--- src/components/dashboard/CarrierPerformance.tsx
+++ src/components/dashboard/CarrierPerformance.tsx
@@ -2,83 +2,59 @@ import React, { useContext, useMemo } from 'react';
 import flow from 'lodash/fp/flow';
 import map from 'lodash/fp/map';
 import get from 'lodash/fp/get';
-import keys from 'lodash/fp/keys';
 import sum from 'lodash/fp/sum';
 import mapValues from 'lodash/fp/mapValues';
 import update from 'lodash/fp/update';
-import values from 'lodash/fp/values';
+import upperCase from 'lodash/fp/upperCase';
+import defaultTo from 'lodash/fp/defaultTo';
 import flatten from 'lodash/fp/flatten';
-import forEach from 'lodash/fp/forEach';
+import toPairs from 'lodash/fp/toPairs';
 import { Card, CardHeader, Divider, CardContent, useTheme, colors } from '@material-ui/core';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import { Doughnut } from 'react-chartjs-2';
 import Carriers from '../../contexts/Carriers';
 import ChartsCircularProgress from './ChartsCircularProgress';
 import filter from 'lodash/fp/filter';
-import logAs from '../../utilities/logAs';
 
 interface Props {
   clientPerformance: any;
   year: number;
 }
 
-const performanceByCarrierByYear = (year: number) => {
-  return flow(
+const performanceByCarrierByYear = (year: number) =>
+  flow(
     get(['TEU']),
-    mapValues(
-      update(String(year), flow(mapValues(flow(flatten, map(flow(get('Amount'), Number)), sum)), values, flatten, sum)),
-    ),
+    mapValues(flow(get(String(year)), map(flow(flatten, map(flow(get('Amount'), Number)), sum)), sum)),
   );
-};
 
 const CarrierPerformance: React.FC<Props> = ({ clientPerformance, year }) => {
   const theme = useTheme();
-  const cs = useContext(Carriers);
+  const carriers = useContext(Carriers);
 
   const data = useMemo(() => {
-    const carriers = flow(get('TEU'), keys)(clientPerformance);
+    const values = flow(
+      performanceByCarrierByYear(year),
+      toPairs,
+      map(update(0, carrierId => carriers?.find(carrier => carrier.id === carrierId))),
+      filter(get(1)),
+    )(clientPerformance);
 
-    const performanceCarrierYear = performanceByCarrierByYear(year)(clientPerformance);
-    // handle odd case where the carrier can have a total sum of zero TEUs
-    // eliminate him from the list
-    let nonZeroTEUCarriers: string[] = [];
-    let datapoints: number[] = [];
-
-    Object.entries(performanceCarrierYear).forEach(([key, value]) => {
-      const yearSum: number = get(String(year))(value);
-      if (yearSum !== 0) {
-        nonZeroTEUCarriers.push(key);
-        datapoints.push(yearSum);
-      }
-    });
-
-    const backgroundColors = map((carrier: string) => {
-      return cs?.find(c => c.id === carrier)?.color || colors.grey[100];
-    })(carriers);
-
-    const total = sum(datapoints);
-
-    const labels = nonZeroTEUCarriers.map(key => {
-      const carrierInfo = filter((element: any) => element.id === key)(cs)[0];
-      return carrierInfo ? carrierInfo.name.toUpperCase() : '';
-    });
-
-    const datasets = [
-      {
-        data: datapoints,
-        backgroundColor: backgroundColors,
-        borderWidth: 2,
-        borderColor: theme.palette.common.white,
-        hoverBorderColor: theme.palette.common.white,
-      },
-    ];
+    const data = map(get(1))(values);
 
     return {
-      datasets,
-      labels: labels,
-      total: total,
+      datasets: [
+        {
+          data,
+          backgroundColor: map(flow(get([0, 'color']), defaultTo(colors.grey[100])))(values),
+          borderWidth: 2,
+          borderColor: theme.palette.common.white,
+          hoverBorderColor: theme.palette.common.white,
+        },
+      ],
+      labels: map(flow(get([0, 'name']), upperCase))(values),
+      total: sum(data),
     };
-  }, [clientPerformance, cs, theme.palette.common.white, year]);
+  }, [clientPerformance, carriers, theme.palette.common.white, year]);
 
   const options = {
     responsive: true,
