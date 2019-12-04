b1fe5a6c7a3363d00d862568fb5aeb90120c343f
Branko Tomic <branko@spicefactory.co>
2019-12-04 16:34:12 +0100

    Adjust sidecharges layout

--- src/components/SideCharges.tsx
+++ src/components/SideCharges.tsx
@@ -17,7 +17,11 @@ import {
   colors,
   Tooltip,
   Grid,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
 } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Carriers from '../contexts/Carriers';
 import Container from './Container';
 import Carrier from '../model/Carrier';
@@ -27,10 +31,25 @@ interface Props {
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+    textTransform: 'uppercase',
+  },
   tableRow: {
     '&:hover': {
       backgroundColor: theme.palette.background.default,
     },
+
+    '& td': {
+      whiteSpace: 'nowrap',
+    },
+  },
+  expansionPanelSummary: {
+    marginBottom: theme.spacing(0),
+  },
+  overflowTable: {
+    overflowX: 'auto',
   },
 }));
 
@@ -40,7 +59,7 @@ const EquipmentSituation: React.FC<Props> = () => {
 
   return (
     <Container>
-      <Box p={2} mt={5} mb={3}>
+      <Box p={2} pb={0} mt={5} mb={1}>
         <Typography variant="h3" gutterBottom>
           Side Charges
         </Typography>
@@ -48,39 +67,43 @@ const EquipmentSituation: React.FC<Props> = () => {
         {/*Find below the overview of all equipment available in Switzerland for your export bookings*/}
         {/*</Typography>*/}
       </Box>
-      <Box my={3}>
+      <Box mb={12}>
         {carriers &&
           carriers.filter(get('sideCharges')).map(({ id, name, sideCharges }) => (
             <Box key={id} my={5}>
               <Box p={2}>
+                {console.log(sideCharges)}
                 <Typography variant="h4">{name || id}</Typography>
               </Box>
               {sideCharges!.groups.map(({ prefix, statements }, i) => (
-                <Box pb={2}>
-                  <Paper>
-                    <Table size="small" aria-label="a dense table">
-                      <TableBody>
-                        {statements.map(({ label, amount, currency, per }, i) => (
-                          <TableRow key={i} className={classes.tableRow}>
-                            <TableCell>
-                              <Grid container>
-                                <Grid item xs={12} sm={6}>
-                                  {[prefix, label].filter(identity).join(' ')}
-                                </Grid>
-                                <Grid item xs={6} sm={3}>
-                                  {currency} {amount}
-                                </Grid>
-                                <Grid item xs={6} sm={3}>
-                                  PER {per}
-                                </Grid>
-                              </Grid>
-                            </TableCell>
-                          </TableRow>
-                        ))}
-                      </TableBody>
-                    </Table>
-                  </Paper>
-                </Box>
+                <ExpansionPanel>
+                  <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    className={classes.expansionPanelSummary}
+                  >
+                    <Typography className={classes.heading}>{prefix ? prefix : 'Other'}</Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                    <Box mt={2} width="100%">
+                      <Paper className={classes.overflowTable}>
+                        <Table size="small" aria-label="a dense table">
+                          <TableBody>
+                            {statements.map(({ label, amount, currency, per }, i) => (
+                              <TableRow key={i} className={classes.tableRow}>
+                                <TableCell>{[label].filter(identity).join(' ')}</TableCell>
+                                <TableCell>
+                                  {currency} {amount} PER {per}
+                                </TableCell>
+                              </TableRow>
+                            ))}
+                          </TableBody>
+                        </Table>
+                      </Paper>
+                    </Box>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
               ))}
             </Box>
           ))}
