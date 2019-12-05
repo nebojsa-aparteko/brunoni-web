8ed4d2412e2f1011c8aa9c4a8d6639b85acfc41a
Jovan Erčić <jovan@spicefactory.co>
2019-12-05 13:25:58 +0100

    Update pickAndRename

--- src/utilities/pickAndRename.ts
+++ src/utilities/pickAndRename.ts
@@ -1,2 +1,5 @@
+import get from 'lodash/fp/get';
+import set from 'lodash/fp/set';
+
 export default (map: { [key: string]: any }) => (value: { [key: string]: any }) =>
-  Object.entries(map).reduce((acc, [name, newName]) => ({ ...acc, [newName]: value[name] }), {});
+  Object.entries(map).reduce((acc, [name, newName]) => set(newName, get(name)(value))(acc as any), {});
