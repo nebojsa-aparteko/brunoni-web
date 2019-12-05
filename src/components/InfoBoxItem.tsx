ecadb8339202666a221ae94f3d598517906ec8d2
Branko Tomic <branko@spicefactory.co>
2019-12-05 14:11:20 +0100

    - Fix responsiveness throughout the app - Adjust quote print styles

--- src/components/InfoBoxItem.tsx
+++ src/components/InfoBoxItem.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box, makeStyles, Theme, Typography } from '@material-ui/core';
 import React, { Fragment } from 'react';
 import { SvgIconProps } from '@material-ui/core/SvgIcon';
 
@@ -11,23 +11,32 @@ interface Props {
   gutterBottom?: boolean;
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+  printText: {
+    ['@media print']: {
+      fontSize: '10px',
+    },
+  },
+}));
+
 const InfoBoxItem: React.FC<Props> = ({ IconComponent, title, label1, label1HTML, label2, gutterBottom }) => {
+  const classes = useStyles();
   const hasLabel1 = label1 || label1HTML;
   return (
     <Fragment>
-      <Typography variant="subtitle2" display="block" gutterBottom>
+      <Typography variant="subtitle2" display="block" gutterBottom className={classes.printText}>
         <Box display="flex" alignItems="center" fontWeight="fontWeightBold">
           {IconComponent && <IconComponent fontSize="small" color="secondary" />}
           <Box ml={IconComponent && '.5em'}>{title}</Box>
         </Box>
       </Typography>
       {hasLabel1 && (
-        <Typography variant="body1" display="block">
+        <Typography variant="body1" display="block" className={classes.printText}>
           {label1HTML ? <span dangerouslySetInnerHTML={label1HTML} /> : <span>{label1}</span>}
         </Typography>
       )}
       {label2 && (
-        <Typography variant="body2" display="block" gutterBottom={gutterBottom}>
+        <Typography variant="body2" display="block" gutterBottom={gutterBottom} className={classes.printText}>
           {label2}
         </Typography>
       )}
---
+++ src/components/quotes/QuoteItemNav.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import formatDate from 'date-fns/format';
+import { Table, TableCell, TableRow, makeStyles } from '@material-ui/core';
+import { Quote } from '../../providers/QuotesEndpoint';
+import TableBody from '@material-ui/core/TableBody';
+import UserRecord from '../../model/UserRecord';
+
+interface Props {
+  quote: Quote;
+  userData: UserRecord;
+}
+
+const useStyles = makeStyles(theme => ({
+  tableCellLabel: {
+    paddingLeft: 0,
+    border: 'none',
+    fontWeight: 700,
+  },
+  tableRow: {
+    [theme.breakpoints.down('sm')]: {
+      display: 'block',
+    },
+  },
+  tableCell: {
+    border: 'none',
+  },
+  tableCellQuoteUserData: {
+    ['@media not print']: {
+      display: 'none',
+    },
+  },
+}));
+
+interface TableRowProps {
+  label: string;
+  content: string;
+  className?: any;
+}
+
+const TableRowData: React.FC<TableRowProps> = ({ label, content, className }) => {
+  const classes = useStyles();
+  return (
+    <TableRow className={classes.tableRow}>
+      <TableCell className={classes.tableCellLabel}>{label}</TableCell>
+      <TableCell className={classes.tableCell}>{content}</TableCell>
+    </TableRow>
+  );
+};
+
+const QuoteItemHeader: React.FC<Props> = ({ quote, userData }) => {
+  const classes = useStyles();
+  return (
+    <Table size="small" aria-label="a dense table">
+      <colgroup>
+        <col style={{ width: '40%' }} />
+        <col style={{ width: '60%' }} />
+      </colgroup>
+      <TableBody>
+        <TableRowData
+          label="Quote For"
+          content={userData?.company.name.toUpperCase() + ', ' + userData?.company.city.toUpperCase()}
+          className={classes.tableCellQuoteUserData}
+        />
+        <TableRowData label="" content="" />
+        <TableRowData label="Quote Date" content={formatDate(quote.dateIssued, 'd. MMMM yyyy')} />
+        <TableRowData label="Quote Number" content={quote.id} />
+        <TableRowData label="Quote Reference" content={quote.clientId} />
+
+        <TableRowData label="Port of Loading" content={`${quote.origin.city}, ${quote.origin.country}`} />
+        <TableRowData label="Port of Discharge" content={`${quote.destination.city}, ${quote.destination.country}`} />
+        <TableRowData label="Carrier" content={quote.carrier.name || quote.carrier.id} />
+
+        {quote.terms
+          .filter(term => term.TermLabel === 'TERMS & CONDITIONS')
+          .map(term => (
+            <TableRowData label="Terms & Conditions" content={term.TermValue} />
+          ))}
+
+        <TableRowData
+          label="Quote Validity"
+          content={`${formatDate(quote.validityPeriod.from, 'd. MMMM')} – ${formatDate(
+            quote.validityPeriod.to,
+            'd. MMMM',
+          )}`}
+        />
+      </TableBody>
+    </Table>
+  );
+};
+
+export default QuoteItemHeader;
