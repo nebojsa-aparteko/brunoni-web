63c7ddb8faf9325109621402d87c479c1df309de
Jovan Erčić <jovan@spicefactory.co>
2019-12-06 16:53:43 +0100

    Add depot location to quote page

--- src/components/quotes/QuoteItemContainers.tsx
+++ src/components/quotes/QuoteItemContainers.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Grid, Typography, ListItem, Box, Chip, makeStyles } from '@material-ui/core';
 import List from '@material-ui/core/List';
+import flow from 'lodash/fp/flow';
+import filter from 'lodash/fp/filter';
+import map from 'lodash/fp/map';
+import get from 'lodash/fp/get';
+import uniqBy from 'lodash/fp/uniqBy';
 import Container from '../../model/Container';
 import CommodityType from '../../model/CommodityType';
+import { getLocationLabel } from '../inputs/LocationInput';
+import PickupLocation from '../../model/PickupLocation';
 
 interface Props {
   containers: Container[];
@@ -26,6 +33,14 @@ const useStyles = makeStyles(theme => ({
 const QuoteItemContainers: React.FC<Props> = ({ containers, commodityTypes }) => {
   const classes = useStyles();
 
+  const locations = useMemo(() => {
+    return flow(
+      map(get('pickupLocation')),
+      filter(location => location.countryCode.trim() !== '0'),
+      uniqBy('id'),
+    )(containers);
+  }, [containers]);
+  console.log('locations', locations);
   return (
     <Grid item xs={12}>
       <Typography variant="subtitle2">
@@ -50,11 +65,25 @@ const QuoteItemContainers: React.FC<Props> = ({ containers, commodityTypes }) =>
         </List>
       </Typography>
       {commodityTypes && commodityTypes.length > 0 && (
-        <Box display="block" alignItems="center">
+        <Box display="block" alignItems="center" mt={2}>
           <Typography variant="subtitle2" style={{ fontWeight: 'bold' }}>
             Commodity:
           </Typography>
-          <Typography variant="body1">{commodityTypes.map(commodityType => commodityType.name)}</Typography>
+          <Typography variant="body1">{commodityTypes.map(commodityType => commodityType.name).join(', ')}</Typography>
+        </Box>
+      )}
+      {locations && locations.length > 0 && (
+        <Box display="block" alignItems="center" mt={2}>
+          <Typography variant="subtitle2" style={{ fontWeight: 'bold' }}>
+            Depot Location:
+          </Typography>
+          <Box>
+            {locations.map((location: PickupLocation) => (
+              <Box>
+                <Typography variant="body1">{getLocationLabel(location)}</Typography>
+              </Box>
+            ))}
+          </Box>
         </Box>
       )}
     </Grid>
