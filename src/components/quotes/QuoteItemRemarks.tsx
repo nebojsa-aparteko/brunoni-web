8a6bf59a3159ed2fab20f660d0a19e16cb59944b
Milos Zikic <milos@spicefactory.co>
2019-12-10 15:56:51 +0100

    On mobile remarks go row by row, while we keep it on print next to each other

--- src/components/quotes/QuoteItemRemarks.tsx
+++ src/components/quotes/QuoteItemRemarks.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, useMediaQuery, useTheme } from '@material-ui/core';
 import Divider from '@material-ui/core/Divider';
 import InfoBoxItem from '../InfoBoxItem';
 import { Remark } from '../../providers/QuoteGroups';
@@ -8,20 +8,25 @@ interface Props {
   remarks: Remark[];
 }
 
-const QuoteItemRemarks: React.FC<Props> = ({ remarks }) => (
-  <Fragment>
-    <Divider />
-    {remarks.map((remark, i) => (
-      <Fragment key={i}>
-        <Grid item md={3} sm={4} xs={4}>
-          <InfoBoxItem title={remark.RemarkTitle} label1={remark.RemarkLabel} />
-        </Grid>
-        <Grid item md={9} sm={8} xs={8}>
-          <InfoBoxItem label1={remark.RemarkText} />
-        </Grid>
-      </Fragment>
-    ))}
-  </Fragment>
-);
+const QuoteItemRemarks: React.FC<Props> = ({ remarks }) => {
+  const theme = useTheme();
+  const isSmAndDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <Fragment>
+      <Divider />
+      {remarks.map((remark, i) => (
+        <Fragment key={i}>
+          <Grid item md={3} sm={4} xs={isSmAndDown ? 12 : 4}>
+            <InfoBoxItem title={remark.RemarkTitle} label1={remark.RemarkLabel} />
+          </Grid>
+          <Grid item md={9} sm={8} xs={isSmAndDown ? 12 : 8}>
+            <InfoBoxItem label1={remark.RemarkText} />
+          </Grid>
+        </Fragment>
+      ))}
+    </Fragment>
+  );
+};
 
 export default QuoteItemRemarks;
