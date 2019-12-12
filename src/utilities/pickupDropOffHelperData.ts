453d32b62485f10da6b983bd8bb430088c2c36c2
Milos Zikic <milos@spicefactory.co>
2019-12-12 09:00:35 +0100

    Remove spain and denmark from EU countries list for pickup/dropoff locations

--- src/utilities/pickupDropOffHelperData.ts
+++ src/utilities/pickupDropOffHelperData.ts
@@ -1,17 +1,7 @@
 // for European ports system needs Pickup/Drop Off location, if origin or destination is not in Europe,
 // this field should not be seen. The following list contains countries that should show PU/DO location field
 // these should all stay in lowercase for comparison reasons
-export const europeanCountries = [
-  'germany',
-  'denmark',
-  'spain',
-  'netherlands',
-  'italy',
-  'belgium',
-  'france',
-  'austria',
-  'switzerland',
-];
+export const europeanCountries = ['germany', 'netherlands', 'italy', 'belgium', 'france', 'austria', 'switzerland'];
 
 // for some cities there should be only <<CITY_NAME>> PORT displayed as available option and other options filtered
 export const doNotShowCities = [
