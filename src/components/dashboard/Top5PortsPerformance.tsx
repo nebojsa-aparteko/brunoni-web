c7757aa2d1d728c4e7877e674082268cb547874c
Milos Zikic <milos@spicefactory.co>
2019-12-16 15:57:53 +0100

    Short country names for Top 5 ports

--- src/components/dashboard/Top5PortsPerformance.tsx
+++ src/components/dashboard/Top5PortsPerformance.tsx
@@ -6,10 +6,12 @@ import get from 'lodash/fp/get';
 import values from 'lodash/fp/values';
 import map from 'lodash/fp/map';
 import flatten from 'lodash/fp/flatten';
+import find from 'lodash/fp/find';
 import groupBy from 'lodash/fp/groupBy';
 import mapValues from 'lodash/fp/mapValues';
 import sum from 'lodash/fp/sum';
 import slice from 'lodash/fp/slice';
+import flatMap from 'lodash/fp/flatMap';
 import toPairs from 'lodash/fp/toPairs';
 import orderBy from 'lodash/fp/orderBy';
 import TableBody from '@material-ui/core/TableBody';
@@ -19,6 +21,7 @@ import Ports from '../../contexts/Ports';
 import filter from 'lodash/fp/filter';
 import Port from '../../model/Port';
 import TextSkeleton from '../TextSkeleton';
+import match from 'autosuggest-highlight/match';
 
 interface Props {
   clientPerformance: any;
@@ -43,6 +46,31 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
+interface StringReplacementPair {
+  match: string;
+  replace: string;
+}
+
+const shortenedCountries: StringReplacementPair[] = [
+  {
+    match: 'UNITED ARAB EMIRATES',
+    replace: 'UAE',
+  },
+  {
+    match: 'UNITED KINGDOM',
+    replace: 'UK',
+  },
+];
+
+function replaceAll(str: string, mapObj: StringReplacementPair[]) {
+  const re = new RegExp(flatMap((pair: StringReplacementPair) => pair.match)(mapObj).join('|'), 'gi');
+
+  return str.replace(re, (matched: string) => {
+    const foundItem = find((pair: StringReplacementPair) => pair.match === matched)(mapObj);
+    return foundItem ? foundItem.replace : matched;
+  });
+}
+
 const extractPortsAggregatedData = (year: number, ports: Port[] | undefined) =>
   flow(
     get('Locations'),
@@ -63,7 +91,7 @@ const extractPortsAggregatedData = (year: number, ports: Port[] | undefined) =>
         map(([key, value]: [any, any]) => {
           if (ports) {
             const portDetails: Port = filter((element: any) => element.id === key)(ports)[0];
-            return [`${portDetails.city}, ${portDetails.country}`, value];
+            return [`${portDetails.city}, ${replaceAll(portDetails.country, shortenedCountries)}`, value];
           } else return [key, value];
         }),
       ),
@@ -86,9 +114,11 @@ const Top5Ports: React.FC<{ data?: Array<[string | React.ReactNode, string | Rea
                 <TableCell className={classNames(classes.tableCell, classes.tableCellFont)} component="th" scope="row">
                   {index + 1}
                 </TableCell>
-                <TableCell component="th" scope="row" className={classes.tableCellFont}>{`${item[0]} (${
-                  item[1] === 'UNITED ARAB EMIRATES' ? 'UAE' : item[1]
-                })`}</TableCell>
+                <TableCell
+                  component="th"
+                  scope="row"
+                  className={classes.tableCellFont}
+                >{`${item[0]} (${item[1]})`}</TableCell>
               </TableRow>
             ))
           : [...Array(5)].map((_, i) => (
