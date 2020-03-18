7ca74012b0d9bc01a998f5550fb5875283c0dcf0
Rados Toskovic <rraaddooss@yahoo.com>
2020-03-18 12:19:11 +0100

    Reverse check list table columns order and improve UI

--- src/components/bookings/ExportChecklist.tsx
+++ src/components/bookings/ExportChecklist.tsx
@@ -4,12 +4,10 @@ import {
   createStyles,
   Theme,
   Table,
-  TableHead,
   TableCell,
   TableRow,
   makeStyles
 } from '@material-ui/core';
-import TableBody from '@material-ui/core/TableBody';
 
 interface Props {
   showCompanyInfo?: boolean;
@@ -35,17 +33,16 @@ const useStyles = makeStyles((theme: Theme) =>
       overflowX: 'auto',
       marginBottom: '1.5em',
     },
-    tableHead: {
-      fontWeight: theme.typography.fontWeightBold,
-    },
     costUnitCell: {
       paddingLeft: 0,
       minWidth: '150px',
     },
     tableRow: {
+      height: '55px',
       '& td': {
         whiteSpace: 'nowrap',
-        textTransform: 'uppercase'
+        textTransform: 'uppercase',
+        padding: '6px 12px',
       },
       ['@media print']: {
         '& td': {
@@ -57,7 +54,8 @@ const useStyles = makeStyles((theme: Theme) =>
       overflowX: 'auto',
     },
     textEmphasized: {
-      color: 'red'
+      color: '#3BADE1',
+      fontWeight: 'bold'
     }
   }),
 );
@@ -122,35 +120,32 @@ const ExportChecklist: React.FC<Props> = ({ showCompanyInfo }) => {
   };
 
   return (
-    <Table className={classes.table} size="small">
-      <TableHead className={classes.tableHead}>
-        <TableRow className={classes.tableRow}>
-          <TableCell>&nbsp;</TableCell>
-          <TableCell align="center">CHECK LIST</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {data && data.map((item: Data, index: number) => {
-          return (
-            <TableRow key={`export-checklist-row-${index}`} selected={(index + 1) % 2 === 0} className={classes.tableRow}>
-              <TableCell>{item.key}</TableCell>
-              {typeof item.value === 'boolean' ? (
-                <TableCell align="center">
-                  <Checkbox
-                    checked={item.value}
-                    onChange={event => handleCheckboxChange(event, item.key)}
-                    disabled={isRegularUser}
-                  />
-                </TableCell>
-              ) : (
-                <TableCell align="center" className={classes.textEmphasized}>
-                  {item.value}
-                </TableCell>
-              )}
-            </TableRow>
-          );
-        })}
-      </TableBody>
+    <Table className={classes.table} size="small" aria-label="a dense table">
+      {data && data.map((item: Data, index: number) => {
+        return (
+          <TableRow
+            key={`export-checklist-row-${index}`}
+            selected={index % 2 === 0}
+            className={classes.tableRow}
+          >
+            {typeof item.value === 'boolean' ? (
+              <TableCell align="center">
+                <Checkbox
+                  checked={item.value}
+                  onChange={event => handleCheckboxChange(event, item.key)}
+                  disabled={isRegularUser}
+                />
+              </TableCell>
+            ) : (
+              <TableCell align="center" className={classes.textEmphasized}>
+                {item.value}
+              </TableCell>
+            )}
+            <TableCell>{item.key}</TableCell>
+            <TableCell>PDF</TableCell>
+          </TableRow>
+        );
+      })}
     </Table>
   );
 };
