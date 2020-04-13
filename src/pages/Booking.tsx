759ba3c6299a9507267ccee433c4e3b9be05a6de
Milos Zikic <milos@spicefactory.co>
2020-04-13 17:47:40 -0400

    Refactor Booking View to abstract where the information is being pulled in away from the display of it

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext, useMemo } from 'react';
 import { RouteComponentProps } from 'react-router';
-import BookingView from '../components/bookings/Booking';
+import BookingView from '../components/bookings/BookingView';
+import Bookings from '../contexts/Bookings';
 
 interface Props extends RouteComponentProps<{ id: string }> {}
 
-const Booking: React.FC<Props> = ({ match }) => <BookingView id={match.params.id} />;
+const Booking: React.FC<Props> = ({ match }) => {
+  const bookings = useContext(Bookings);
+
+  const bookingId = match.params.id;
+  // FIXME change this to firebase.get() specific booking instance
+  const booking = useMemo(() => bookings?.find(booking => booking.id === bookingId), [bookings, bookingId]);
+
+  return <BookingView booking={booking} />;
+};
 
 export default Booking;
