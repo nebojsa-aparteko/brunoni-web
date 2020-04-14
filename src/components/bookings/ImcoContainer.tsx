f4ef4ee1826ba824ec4e68bc066320f3bfc3ca1e
Milos Zikic <milos@spicefactory.co>
2020-04-14 07:51:28 -0400

    IMCO container

--- src/components/bookings/ImcoContainer.tsx
+++ src/components/bookings/ImcoContainer.tsx
@@ -5,12 +5,13 @@ import { IMCOField } from '../../model/Booking';
 interface Prop {
   detail: IMCOField;
 }
+
 const ImcoContainer: React.FC<Prop> = ({ detail }) => {
   return (
     <Fragment>
       <TableRowData
         label="IMCO"
-        content={`${detail.IMOClass ? `${detail.IMOClass}` : ''} ${detail.UNNumber ? `/ ${detail.UNNumber}` : ''}  ${
+        content={`${detail.IMOClass || ''} ${detail.UNNumber ? `/ ${detail.UNNumber}` : ''}  ${
           detail.PackingNumber ? `/ ${detail.PackingNumber}` : ''
         } ${detail.FlashPoint ? `/ ${detail.FlashPoint}` : ''}`}
       />
