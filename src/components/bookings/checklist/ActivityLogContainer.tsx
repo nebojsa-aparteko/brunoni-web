5a7e7189f2aa0adf77d800bd5d4e14c08f466614
Milos Zikic <milos@spicefactory.co>
2020-04-15 11:31:27 -0400

    Start on activity log restructure

---
+++ src/components/bookings/checklist/ActivityLogContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ActivityLogView from './ActivityLogView';
+
+const ActivityLogContainer: React.FC = () => {
+  //TODO Fetching bla bla bla
+
+  // {INVOICE}
+  // Text. matcher replace({INVOICE}}
+
+  return <ActivityLogView />;
+};
---
+++ src/components/bookings/checklist/ActivityLogView.tsx
@@ -0,0 +1,5 @@
+import React, { Fragment } from 'react';
+
+const ActivityLogView: React.FC = () => <Fragment />;
+
+export default ActivityLogView;
