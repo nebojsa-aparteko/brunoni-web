ec6c9cedb0b05924ffd2e1645580015fce877d7a
Milos Zikic <milos@spicefactory.co>
2020-04-27 15:40:23 -0400

    Cleanup unneeded imports

--- src/providers/QuotesProvider.tsx
+++ src/providers/QuotesProvider.tsx
@@ -1,28 +1,122 @@
-import React from 'react';
-import QuotesContext from '../contexts/Quotes';
-import FirestoreCollectionProvider from './FirestoreCollection';
+import React, { createContext, Reducer, useContext, useMemo, useReducer } from 'react';
 import useUser from '../hooks/useUser';
+import ActingAs from '../contexts/ActingAs';
+import { Action, ContextFilters, reducer } from './filterActions';
+import useFirestoreCollection from '../hooks/useFirestoreCollection';
+import { Quote } from './QuoteGroupsProvider';
+import { subMonths } from 'date-fns';
 
 interface Props {
   children: React.ReactNode;
 }
 
+export type QuoteDispatch = (action: Action) => void;
+
+interface QuoteContextFilters extends ContextFilters {}
+
+const defaultFilters = {
+  dateRange: { startDate: subMonths(new Date(), 2), endDate: new Date() },
+} as QuoteContextFilters;
+
+export const QuotesContext = createContext<[Quote[], QuoteContextFilters] | [undefined, QuoteContextFilters]>([
+  undefined,
+  defaultFilters,
+]);
+
+const QuotesFilterDispatchContext = createContext<QuoteDispatch | undefined>(undefined);
+
 const QuotesProvider: React.FC<Props> = ({ children }) => {
   const userRecord = useUser()[1];
+  const actingAs = useContext(ActingAs)[0];
 
-  const query = userRecord?.alphacomClientId
-    ? (collection: firebase.firestore.CollectionReference) =>
-        collection
-          .where('clientId', '==', userRecord!.alphacomClientId)
-          .orderBy('dateIssued', 'desc')
-          .limit(800)
-    : (collection: firebase.firestore.CollectionReference) => collection.orderBy('dateIssued', 'desc').limit(800);
+  const [filters, dispatch] = useReducer<Reducer<QuoteContextFilters, Action>>(reducer, defaultFilters);
+
+  // in case of admins set assignee filter automatically
+  // TODO activate this when it starts having sense :)
+  // useEffect(() => {
+  //   if (userRecord && userRecord.isAdmin && !actingAs && dispatch) {
+  //     dispatch({type: 'set', field: 'assignee', value: userRecord})
+  //   }
+  // }, [userRecord, actingAs, dispatch]);
+
+  const query = useMemo(
+    () => (collection: firebase.firestore.CollectionReference) => {
+      let query = collection.orderBy('dateIssued', 'desc');
+
+      if (actingAs && userRecord?.alphacomClientId) {
+        query = query.where('clientId', '==', userRecord!.alphacomClientId);
+      }
+
+      // admins have different filters, clients should default to seeing all
+      // TODO add archived when ready
+      // if (!actingAs) {
+      //   query = query.where('archived', '==', filters.archived);
+      // }
+
+      if (filters.dateRange?.startDate) {
+        query = query.where('dateIssued', '>=', filters.dateRange.startDate);
+      }
+      if (filters.dateRange?.endDate) {
+        query = query.where('dateIssued', '<=', filters.dateRange.endDate);
+      }
+
+      if (filters.assignee) {
+        const splituserId = filters.assignee.alphacomId.split('-');
+        const normalizedUserId = splituserId[0] + '-' + Number(splituserId[1]);
+        query = query.where('assignee', '==', normalizedUserId);
+      }
+
+      if (filters.originPort) {
+        query = query.where('origin', '==', filters.originPort.id);
+      }
+
+      if (filters.destinationPort) {
+        query = query.where('destination', '==', filters.destinationPort.id);
+      }
+
+      if (filters.clientFilter) {
+        query = query.where('clientId', '==', filters.clientFilter.id);
+      }
+
+      return query;
+    },
+    [userRecord, filters, actingAs],
+  );
+
+  const quotesSnapshot = useFirestoreCollection('quotes', query);
+
+  const quotesResult = useMemo(() => {
+    const quotes = quotesSnapshot?.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      } as Quote;
+    }) as Quote[] | undefined;
+
+    return quotes as any;
+  }, [quotesSnapshot]);
 
   return (
-    <FirestoreCollectionProvider name="quotes" query={query} context={QuotesContext}>
-      {children}
-    </FirestoreCollectionProvider>
+    <QuotesContext.Provider value={[quotesResult, filters]}>
+      <QuotesFilterDispatchContext.Provider value={dispatch}>{children}</QuotesFilterDispatchContext.Provider>
+    </QuotesContext.Provider>
   );
 };
 
 export default QuotesProvider;
+
+export const useQuotesContext = () => {
+  const context = React.useContext(QuotesContext);
+  if (context === undefined) {
+    throw new Error('useQuotesContext must be used within a QuotesProvider');
+  }
+  return context;
+};
+
+export const useQuotesFilterDispatch = () => {
+  const context = React.useContext(QuotesFilterDispatchContext);
+  if (context === undefined) {
+    throw new Error('useQuotesFilterDispatch must be used within a QuotesProvider');
+  }
+  return context;
+};
