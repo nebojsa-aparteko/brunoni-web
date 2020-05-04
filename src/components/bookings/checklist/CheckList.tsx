065847c977a4427d436ac4a1f4f3414090bb318c
Vladan Lukic <vladan@spicefactory.co>
2020-05-04 14:59:11 +0200

    Remove AppBar and Tabs from checklist

--- src/components/bookings/checklist/CheckList.tsx
+++ src/components/bookings/checklist/CheckList.tsx
@@ -1,5 +1,15 @@
 import React, { Fragment, useCallback, useContext, useMemo } from 'react';
-import { AppBar, Box, Card, CardActions, CardContent, Container, Paper, Tab, Tabs } from '@material-ui/core';
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Container,
+  Paper,
+  Divider,
+  Typography,
+} from '@material-ui/core';
 import { Booking } from '../../../model/Booking';
 import ChecklistItemRow from './ChecklistItemRow';
 import ChartsCircularProgress from '../../dashboard/ChartsCircularProgress';
@@ -13,28 +23,6 @@ interface CheckListProps {
   booking: Booking;
 }
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: any;
-  value: any;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Box
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && children}
-    </Box>
-  );
-}
-
 export const editRestriction = (date: Date) =>
   differenceInMilliseconds(new Date(), date) <= Number(process.env.EDIT_RESTRICTION_TIME) || 600000;
 
@@ -42,12 +30,6 @@ const CheckList: React.FC<CheckListProps> = ({ booking }) => {
   const actingAs = useContext(ActingAs)[0];
 
   const checklistItems = useChecklist(booking.id);
-
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-    setValue(newValue);
-  };
 
   if (!checklistItems) {
     return (
@@ -62,29 +44,30 @@ const CheckList: React.FC<CheckListProps> = ({ booking }) => {
   return (
     <Fragment>
       <ActivityLogProvider>
-        <AppBar position="static">
-          <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-            <Tab label="Checklist" id="simple-tab-0" aria-controls="simple-tabpanel-0" />
-          </Tabs>
-        </AppBar>
-        <TabPanel value={value} index={0}>
-          <Card>
-            <CardContent>
-              <Box display="flex" flexDirection="column" style={{ flex: 1 }}>
-                {checklistItems.map(item => (
-                  <ChecklistItemRow
-                    key={`chkitem-${booking.id}-${item.id}`}
-                    checklistItem={item}
-                    isAdmin={!actingAs}
-                    booking={booking}
-                  />
-                ))}
-              </Box>
-            </CardContent>
-            <CardActions>Hint: you can drag files onto the checklist items to attach them</CardActions>
-          </Card>
-          <ActivityLogContainer bookingId={booking.id} isAdmin={!actingAs} />
-        </TabPanel>
+        <Card>
+          <CardHeader
+            title={
+              <Typography component="h2" variant="h2">
+                Checklist
+              </Typography>
+            }
+          />
+          <Divider />
+          <CardContent>
+            <Box display="flex" flexDirection="column" style={{ flex: 1 }}>
+              {checklistItems.map(item => (
+                <ChecklistItemRow
+                  key={`chkitem-${booking.id}-${item.id}`}
+                  checklistItem={item}
+                  isAdmin={!actingAs}
+                  booking={booking}
+                />
+              ))}
+            </Box>
+          </CardContent>
+          <CardActions>Hint: you can drag files onto the checklist items to attach them</CardActions>
+        </Card>
+        <ActivityLogContainer bookingId={booking.id} isAdmin={!actingAs} />
       </ActivityLogProvider>
     </Fragment>
   );
