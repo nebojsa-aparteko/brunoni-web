5bfef8fc9f1904798d1ac960c3b66d2a787504fc
Filip Antic <filip.antic@spicefactory.co>
2020-05-21 10:10:27 +0200

    Show only export bookings on Load list

--- src/hooks/useContainers.ts
+++ src/hooks/useContainers.ts
@@ -3,11 +3,13 @@ import { useCallback } from 'react';
 import useFirestoreCollection from './useFirestoreCollection';
 import LoadListContainerModel from '../model/LoadListContainerModel';
 import subDays from 'date-fns/subDays';
+import { BookingCategory } from '../model/Booking';
 
 export default function useContainers(q?: () => any) {
   const query = useCallback(
     q =>
       q
+        .where('category', '==', BookingCategory.Export)
         .where('ets', '>=', subDays(new Date(), 1))
         .orderBy('ets', 'asc')
         .orderBy('bookingId', 'asc'),
