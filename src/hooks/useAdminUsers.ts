603f3628f0fa6395f19d9c241bf96452f5a57275
Filip Antic <filip.antic@spicefactory.co>
2020-05-22 23:50:23 +0200

    Show only admins with isAdmin flag

--- src/hooks/useAdminUsers.ts
+++ src/hooks/useAdminUsers.ts
@@ -7,6 +7,8 @@ export default function useAdminUsers(roles: string[] = ADMIN_ROLES) {
   const users = useContext(UserRecordsContext);
 
   return users
-    ? users.filter(user => roles.indexOf(user.role!) !== -1).sort((a, b) => (a.firstName! >= b.firstName! ? 1 : -1))
+    ? users
+        .filter(user => roles.indexOf(user.role!) !== -1 && user.isAdmin)
+        .sort((a, b) => (a.firstName! >= b.firstName! ? 1 : -1))
     : [];
 }
