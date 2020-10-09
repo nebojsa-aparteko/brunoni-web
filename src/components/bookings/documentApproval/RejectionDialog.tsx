daca76054279150755d998c9f4ddaa6f6e555209
Marko Nenadović <marko.nenadovic@spicefactory.co>
2020-10-09 12:32:16 +0200

    Refactor the code and remove the file comparison selector for shipping instructions

---
+++ src/components/bookings/documentApproval/RejectionDialog.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  Box,
+  Button,
+  createStyles,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import CommentInput from '../../CommentInput';
+import { Booking } from '../../../model/Booking';
+import { ChecklistItem } from '../checklist/ChecklistItemModel';
+import { RejectionInput } from './RejectionModal';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    closeModal: {
+      position: 'absolute',
+      top: '5px',
+      right: '12px',
+      width: '47px',
+      height: '47px',
+    },
+    dialogContent: {
+      display: 'flex',
+      flexFlow: 'column',
+    },
+  }),
+);
+
+const RejectionDialog: React.FC<Props> = ({
+  isOpen,
+  booking,
+  handleClose,
+  checklistItem,
+  onReject,
+  rejectionInput,
+  onRejectionInputChange,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Dialog open={isOpen} onClose={handleClose} aria-labelledby="dialog-title-check-list" maxWidth="md" fullWidth>
+      <Box>
+        <DialogTitle disableTypography id="dialog-title-check-list">
+          <Typography variant="h4">{`Please enter needed correction on ${checklistItem.label} document`}</Typography>
+          <IconButton onClick={handleClose} className={classes.closeModal}>
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent className={classes.dialogContent}>
+          <CommentInput booking={booking} onInputChange={onRejectionInputChange} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" variant="outlined" autoFocus>
+            Cancel
+          </Button>
+          <Button
+            onClick={onReject}
+            variant="contained"
+            color="primary"
+            disabled={!rejectionInput || rejectionInput?.messagePlain.length < 1}
+          >
+            Request amendment
+          </Button>
+        </DialogActions>
+      </Box>
+    </Dialog>
+  );
+};
+
+export default RejectionDialog;
+
+interface Props {
+  isOpen: boolean;
+  handleClose: () => void;
+  booking: Booking;
+  checklistItem: ChecklistItem;
+  onReject: () => void;
+  rejectionInput: RejectionInput | undefined;
+  onRejectionInputChange: (input: RejectionInput) => void;
+}
---
+++ src/components/pdfViewer/PDFViewer.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+import { Box, Fab, Grid } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import { PDFDocumentProxy } from 'pdfjs-dist';
+import { ChecklistItemValueDocument } from '../bookings/checklist/ChecklistItemModel';
+import DocumentView from './DocumentView';
+
+const PDFViewer: React.FC<Props> = ({ file }) => {
+  const [numPages, setNumPages] = useState<number>(0);
+  const [scale, setScale] = useState<number>(1);
+
+  const onDocumentLoadSuccess = (determinedNumPages: PDFDocumentProxy) => {
+    setNumPages(determinedNumPages.numPages);
+  };
+
+  const changeScale = (offset: number) => {
+    setScale(scale + offset);
+  };
+
+  return (
+    <Box flex={1}>
+      <Box style={{ position: 'absolute', bottom: 64, right: 32, zIndex: 200 }}>
+        <Grid container direction="column-reverse" spacing={1}>
+          <Grid item>
+            <Fab size="medium" disabled={scale <= 0.4} onClick={() => changeScale(scale <= 1.5 ? -0.1 : -0.5)}>
+              <RemoveIcon />
+            </Fab>
+          </Grid>
+          <Grid item>
+            <Fab size="medium" disabled={scale >= 5} onClick={() => changeScale(scale < 1.5 ? 0.1 : 0.5)}>
+              <AddIcon />
+            </Fab>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box style={{ position: 'absolute', top: 64, right: 32, zIndex: 200 }}>
+        <Fab href={file.url} size="small" download={file.name} target="_blank">
+          <GetAppIcon />
+        </Fab>
+      </Box>
+      <DocumentView scale={scale} file={file} onDocumentLoadSuccess={onDocumentLoadSuccess} numPages={numPages} />
+    </Box>
+  );
+};
+
+export default PDFViewer;
+
+interface Props {
+  file: ChecklistItemValueDocument;
+}
