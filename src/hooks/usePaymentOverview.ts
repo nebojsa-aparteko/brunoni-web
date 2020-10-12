bc215fb365d4cbc81775838f25b1e070e75747c0
filipantic <filip.antic@spicefactory.co>
2020-10-12 13:34:17 +0200

    Add Weekly payment filters

---
+++ src/hooks/usePaymentOverview.ts
@@ -0,0 +1,38 @@
+import { useContext, useMemo } from 'react';
+import { UserRole } from '../model/Task';
+import ActingAs from '../contexts/ActingAs';
+import UserRecordContext from '../contexts/UserRecordContext';
+import firebase from '../firebase';
+import pick from 'lodash/fp/pick';
+import { UserRecordMinProperties } from '../model/UserRecord';
+import WeeklyPayment from '../model/WeeklyPayment';
+import useFirestoreCollection from './useFirestoreCollection';
+import { Team } from '../model/Teams';
+import { useWeeklyPaymentFilterProviderContext } from '../providers/WeeklyPaymentFilterProvider';
+
+export default () => {
+  const [filters, _] = useWeeklyPaymentFilterProviderContext();
+  const { carrier, paymentDate } = filters;
+  const [actingAs] = useContext(ActingAs);
+  const userRecord = useContext(UserRecordContext);
+
+  const query = useMemo(
+    () => (collection: firebase.firestore.Query) => {
+      let query = collection.where('resolved', '==', false).where('show', '==', true);
+      if (paymentDate) {
+        // query = query.where('assignedUser', '==', pick(UserRecordMinProperties)(assignee));
+      }
+      if (actingAs) {
+        query = query.where('assignedUser', '==', pick(UserRecordMinProperties)(userRecord));
+      }
+      return query;
+    },
+    [filters, paymentDate, UserRecordMinProperties, pick, carrier, UserRole, actingAs, userRecord],
+  );
+
+  const teamsCollection = useFirestoreCollection('teams', query);
+
+  return teamsCollection?.docs.map(doc => {
+    return { id: doc.id, ...doc.data() } as Team;
+  }) as WeeklyPayment[];
+};
---
+++ src/providers/WeeklyPaymentFilterProvider.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, Dispatch, SetStateAction, useState } from 'react';
+import Carrier from '../model/Carrier';
+import { Currency } from '../model/WeeklyPayment';
+
+export interface WeeklyPaymentFilterContext {
+  carrier?: Carrier;
+  paymentDate?: Date;
+  currency: Currency;
+}
+
+export const TASK_FILTERS_INITIAL_STATE = { currency: Currency.USD } as WeeklyPaymentFilterContext;
+
+const WeeklyPaymentFilterProviderContext = createContext<
+  [WeeklyPaymentFilterContext, Dispatch<SetStateAction<WeeklyPaymentFilterContext>> | undefined]
+>([TASK_FILTERS_INITIAL_STATE, undefined]);
+
+const WeeklyPaymentFilterProvider = (props: any) => {
+  const [state, setState] = useState<WeeklyPaymentFilterContext>({
+    ...TASK_FILTERS_INITIAL_STATE,
+  });
+
+  return (
+    <WeeklyPaymentFilterProviderContext.Provider value={[state, setState]}>
+      {props.children}
+    </WeeklyPaymentFilterProviderContext.Provider>
+  );
+};
+
+export const useWeeklyPaymentFilterProviderContext = () => {
+  const context = React.useContext(WeeklyPaymentFilterProviderContext);
+  if (context === undefined) {
+    throw new Error('useWeeklyPaymentFilterProviderContext must be used within a WeeklyProviderFilterProvider');
+  }
+  return context;
+};
+
+export default WeeklyPaymentFilterProvider;
