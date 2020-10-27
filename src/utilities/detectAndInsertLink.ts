d2b1d4caa5b6117d975f63f4d16c68c7f39169c3
Marko Nenadović <marko.nenadovic@spicefactory.co>
2020-10-27 14:56:43 +0100

    Change the link to open in a new tab

--- src/utilities/detectAndInsertLink.ts
+++ src/utilities/detectAndInsertLink.ts
@@ -1,4 +1,4 @@
 export const detectAndInsertLink = (content: string) => {
   const reg = new RegExp(/(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-|~)+)/g);
-  return content.replace(reg, "<a href='$1$2'>$1$2</a>");
+  return content.replace(reg, '<a href=\'$1$2\' target="_blank">$1$2</a>');
 };
