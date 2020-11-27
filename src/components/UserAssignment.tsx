3a475c10f0e984634580c1f11f3d175087a4d7be
Marko Nenadović <marko.nenadovic@spicefactory.co>
2020-11-27 10:21:39 +0100

    Remove the option for clients to assign users to quote groups

--- src/components/UserAssignment.tsx
+++ src/components/UserAssignment.tsx
@@ -1,11 +1,12 @@
 import UserRecord, { CUSTOMER_FACING_ROLES } from '../model/UserRecord';
 import UserInput from './inputs/UserInput';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import useAdminUsers from '../hooks/useAdminUsers';
 import { Box } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import PersonIcon from '@material-ui/icons/Person';
 import ClearIcon from '@material-ui/icons/Clear';
+import ActingAs from '../contexts/ActingAs';
 
 interface Props {
   value?: UserRecord;
@@ -14,17 +15,28 @@ interface Props {
 
 const UserAssignment: React.FC<Props> = ({ value, onChange, ...other }) => {
   const [changeInProgress, setChangeInProgress] = useState(false);
+  const [actingAs] = useContext(ActingAs);
 
   const assignableUsers = useAdminUsers(CUSTOMER_FACING_ROLES);
 
   return (
     <Box display="flex" flexDirection="row" justifyContent="flex-end" {...other}>
       {!changeInProgress ? (
-        <div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            flexWrap: 'wrap',
+          }}
+        >
           {value?.firstName} {value?.lastName}
-          <IconButton aria-label="assign" onClick={() => setChangeInProgress(true)}>
-            <PersonIcon />
-          </IconButton>
+          {actingAs ? (
+            <PersonIcon style={{ margin: 4 }} />
+          ) : (
+            <IconButton aria-label="assign" onClick={() => setChangeInProgress(true)}>
+              <PersonIcon />
+            </IconButton>
+          )}
         </div>
       ) : (
         <Box display="flex" style={{ minWidth: '300px' }}>
