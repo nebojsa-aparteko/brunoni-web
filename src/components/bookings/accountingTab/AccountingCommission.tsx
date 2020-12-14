52af10c4ee3e41d989d4e86fce71f656fb6f0565
Marko Nenadović <marko.nenadovic@spicefactory.co>
2020-12-14 16:28:18 +0100

    Add Commissions to accounting tab

---
+++ src/components/bookings/accountingTab/AccountingCommission.tsx
@@ -0,0 +1,47 @@
+import { Box, Paper, Typography } from '@material-ui/core';
+import { formatDateSafe } from '../../../utilities/formattingHelpers';
+import currencyFormatter from '../../../utilities/currencyFormatter';
+import Commission, { CommissionStatus } from '../../../model/Commission';
+import React from 'react';
+
+const AccountingCommission = ({ commission }: AccountingCommissionProps) => {
+  return (
+    <Paper
+      style={{
+        backgroundColor: 'rgba(255,0,0,0.05)',
+        paddingLeft: 16,
+        paddingRight: 16,
+        paddingTop: 12,
+        paddingBottom: 12,
+        margin: 4,
+      }}
+    >
+      <Box flex={1} display="flex" flexDirection="row" justifyContent="space-between">
+        <Typography variant={'h5'}>{formatDateSafe(commission.dueDate, 'd. MMMM yyyy.')}</Typography>
+        <Typography variant={'h5'}>
+          {'Amount: '.concat(currencyFormatter(commission.currency)(commission.amount))}
+        </Typography>
+        <Typography
+          variant={'h5'}
+          style={{
+            fontWeight: 700,
+            color:
+              commission.status === CommissionStatus.PAID
+                ? 'rgba(0,200,81)'
+                : commission.status === CommissionStatus.INVOICED
+                ? '#b186df'
+                : '#000',
+          }}
+        >
+          {commission.status}
+        </Typography>
+      </Box>
+    </Paper>
+  );
+};
+
+interface AccountingCommissionProps {
+  commission: Commission;
+}
+
+export default AccountingCommission;
--- src/components/bookings/accountingTab/AccountingTabContent.tsx
+++ src/components/bookings/accountingTab/AccountingTabContent.tsx
@@ -1,15 +1,19 @@
-import { Box, Container, Typography } from '@material-ui/core';
+import { Box, Container, Divider, Typography } from '@material-ui/core';
 import React from 'react';
 import { Booking } from '../../../model/Booking';
 import ChartsCircularProgress from '../../dashboard/ChartsCircularProgress';
 import usePaymentOverview from '../../../hooks/usePaymentOverview';
 import AccountingWeeklyPayment from './AccountingWeeklyPayment';
 import WeeklyPayment from '../../../model/WeeklyPayment';
+import useCommissions from '../../../hooks/useCommissions';
+import Commission from '../../../model/Commission';
+import AccountingCommission from './AccountingCommission';
 
 const AccountingTabContent = ({ booking, updateComponent }: AccountingTabContentProps) => {
   const weeklyPayments = usePaymentOverview(booking.id) as WeeklyPayment[];
+  const commissions = useCommissions(booking.id) as Commission[];
 
-  if (!weeklyPayments) {
+  if (!weeklyPayments || !commissions) {
     return (
       <Container>
         <ChartsCircularProgress />
@@ -19,15 +23,28 @@ const AccountingTabContent = ({ booking, updateComponent }: AccountingTabContent
 
   return (
     <Box display="flex" flex={1} flexDirection="column" px={0} style={{ listStyle: 'none' }}>
-      {weeklyPayments.length > 0 ? (
-        weeklyPayments.map(payment => (
-          <AccountingWeeklyPayment
-            key={payment.reference}
-            payment={payment}
-            booking={booking}
-            updateComponent={updateComponent}
-          />
-        ))
+      {(weeklyPayments && weeklyPayments.length > 0) || (commissions && commissions.length > 0) ? (
+        <React.Fragment>
+          {weeklyPayments.map(payment => (
+            <AccountingWeeklyPayment
+              key={payment.reference}
+              payment={payment}
+              booking={booking}
+              updateComponent={updateComponent}
+            />
+          ))}
+          {weeklyPayments.length > 0 && commissions.length > 0 && <Divider style={{ margin: 8 }} />}
+          {commissions.length > 0 && (
+            <React.Fragment>
+              <Typography variant="body2" style={{ margin: 6, marginTop: 0 }}>
+                Commissions:
+              </Typography>
+              {commissions.map(commission => (
+                <AccountingCommission key={commission.id} commission={commission} />
+              ))}
+            </React.Fragment>
+          )}
+        </React.Fragment>
       ) : (
         <Box flex={1} p={2}>
           <Typography>There are currently no weekly payments for this booking.</Typography>
