e705de5b1ff2f3586d1776ca174047f87a73e417
Nikola Lozo <nikola.lozo@spicefactory.co>
2021-04-19 14:14:18 +0200

    Add FOB and VGM booking request

--- src/components/bookingRequests/BookingRequestPortTerms.tsx
+++ src/components/bookingRequests/BookingRequestPortTerms.tsx
@@ -3,8 +3,9 @@ import { createStyles, makeStyles, Table, Theme } from '@material-ui/core';
 import React from 'react';
 import { TableRowData } from '../bookings/ContainerDetails';
 import { BookingRequest } from '../../model/BookingRequest';
+import { useClientById } from '../../hooks/useClient';
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     portTermsTable: {
       width: '100%',
@@ -14,6 +15,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const BookingRequestPortTerms: React.FC<Props> = ({ bookingRequest }) => {
   const classes = useStyles();
+  const client = useClientById(bookingRequest.createdBy.alphacomClientId);
 
   return (
     <Table size="small" aria-label="a dense table" className={classes.portTermsTable}>
@@ -23,10 +25,15 @@ const BookingRequestPortTerms: React.FC<Props> = ({ bookingRequest }) => {
       </colgroup>
       <TableBody>
         {bookingRequest.schedule?.OriginInfo.Port.PortAgent && (
-          <TableRowData label={'Liner Port Agent'} content={bookingRequest.schedule?.OriginInfo.Port.PortAgent} />
+          <React.Fragment>
+            <TableRowData label={'Liner Port Agent'} content={bookingRequest.schedule?.OriginInfo.Port.PortAgent} />
+            <TableRowData
+              label={'FOB Delivery By'}
+              content={bookingRequest.schedule?.OriginInfo.Port.PortAgent.split('<br/>')[0]}
+            />
+          </React.Fragment>
         )}
-        {/*<TableRowData label={'FOB Delivery By'} content={portTerms.FOBDeliveryBy} />*/}
-        {/*<TableRowData label={'VGM Submission By'} content={portTerms.VGMSubmByTxt} />*/}
+        {client && <TableRowData label={'VGM Submission By'} content={client.name + ', ' + client.city} />}
       </TableBody>
     </Table>
   );
