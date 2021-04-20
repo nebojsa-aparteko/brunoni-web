9f6cdb76deef05a279a101d211f80251a7bcf2d8
filipantic <filip.antic@spicefactory.co>
2021-04-20 13:07:20 +0200

    Remove negative notifications count

--- src/components/notifications/NotificationsButton.tsx
+++ src/components/notifications/NotificationsButton.tsx
@@ -36,7 +36,7 @@ const NotificationsButton: React.FC<IconButtonProps> = props => {
         style={{ padding: 8 }}
         {...props}
       >
-        <Badge badgeContent={userRecord?.unreadNotifications || 0} color="secondary">
+        <Badge badgeContent={getNotificationCount(userRecord?.unreadNotifications)} color="secondary">
           <NotificationsIcon color="primary" fontSize="small" />
         </Badge>
       </IconButton>
@@ -61,3 +61,7 @@ const NotificationsButton: React.FC<IconButtonProps> = props => {
 };
 
 export default NotificationsButton;
+
+const getNotificationCount = (unreadNotifications?: number) => {
+  return unreadNotifications ? (unreadNotifications > 0 ? unreadNotifications : 0) : 0;
+};
