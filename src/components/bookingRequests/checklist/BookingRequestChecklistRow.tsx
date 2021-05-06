f16cc321bbefcf853c76c782343255c18ebbab66
filipantic <filip.antic@spicefactory.co>
2021-05-06 08:09:47 +0200

    Add internal storage for booking request checklist

--- src/components/bookingRequests/checklist/BookingRequestChecklistRow.tsx
+++ src/components/bookingRequests/checklist/BookingRequestChecklistRow.tsx
@@ -11,12 +11,16 @@ import {
 import { ActivityLogItem, ActivityType, PaymentActivityData } from '../../bookings/checklist/ActivityModel';
 import { MentionItem } from 'react-mentions';
 import { flow, isNil, omitBy } from 'lodash/fp';
-import React, { useCallback, useContext } from 'react';
+import React, { useCallback, useContext, useMemo, useState } from 'react';
 import UserRecordContext from '../../../contexts/UserRecordContext';
 import { useSnackbar } from 'notistack';
 import firebase from '../../../firebase';
 import DoneIcon from '@material-ui/icons/Done';
 import { BookingRequest } from '../../../model/BookingRequest';
+import { useDropzone } from 'react-dropzone';
+import { StoredDocument } from '../../../model/Booking';
+import { makeContentDispositionFileName } from '../../DropZone';
+import { fileWithExt } from '../../bookings/checklist/ChecklistItemRow';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -25,6 +29,17 @@ const useStyles = makeStyles(() =>
       '&:focus': {
         outline: 'none',
       },
+    },
+    dropZone: {
+      border: '1px dashed #ccc',
+      cursor: 'pointer',
+      borderColor: '#999',
+      '&:focus': {
+        outline: 'none',
+      },
+    },
+    dropZoneHint: {
+      backgroundColor: 'rgba(0,0,0,0.6)',
     },
   }),
 );
@@ -169,6 +184,117 @@ const BookingRequestChecklistRow = ({ bookingRequest, checklistItem, isAdmin }: 
     },
     [checklistItemCheckedHandler, storeActivity],
   );
+
+  const checklistItemFileAddedHandler = useCallback(
+    (addedFiles: ChecklistItemValueDocument[]) => {
+      const newDocuments = (checklistItem.valuesAdmin || []).concat(addedFiles);
+      return saveChecklistChanges('valuesAdmin', newDocuments)
+        .then(_ =>
+          addActivityItem(
+            bookingRequest!.id!,
+            createActivityObject({
+              changeType: ActivityChangeType.ADD_FILE,
+              by: getActivityLogUserData(),
+              checklistItem: checklistItem,
+              documents: addedFiles,
+            }),
+          ),
+        )
+        .catch(error => console.error('Error saving new document list', error));
+    },
+    [bookingRequest, checklistItem, getActivityLogUserData, saveChecklistChanges],
+  );
+  const storageBasePath = useMemo((): string => {
+    return ['booking-documents', 'clients', '/', 'bookings', '', checklistItem.id].join('/');
+  }, [bookingRequest, checklistItem]);
+
+  // status indicators
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [uploadTask, setUploadTask] = useState<firebase.storage.UploadTask>(); // add some control to uploads so that users can cancel
+
+  const saveFiles = useCallback(
+    async (files: File[]): Promise<any> => {
+      const uploadFile = async (file: File): Promise<any> => {
+        return new Promise((resolve, reject) => {
+          const fileWithExtension = fileWithExt(file.name);
+          const storedFileName = `${fileWithExtension.name}_${new Date().getTime()}.${fileWithExtension.ext}`;
+          let path = [storageBasePath, storedFileName].join('/');
+
+          let storageRef = firebase.storage().ref(encodeURI(path));
+          let uploadTask = storageRef.put(file);
+          setUploadTask(uploadTask);
+
+          uploadTask.on(
+            firebase.storage.TaskEvent.STATE_CHANGED,
+            snapshot => {
+              console.log('progress: ', (snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+              setUploadProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+              // in progress
+              // if(snapshot.state === firebase.storage.TaskState.RUNNING) {
+              //   // ex. calculate progress
+              // }
+            },
+            error => {
+              setUploadProgress(0);
+              reject(error);
+              enqueueSnackbar(<Typography color="inherit">Failed to upload file - {error.message}!</Typography>, {
+                variant: 'error',
+                autoHideDuration: 1000,
+              });
+            },
+            () => {
+              setUploadProgress(0);
+              // success
+              storageRef.updateMetadata({
+                contentDisposition: makeContentDispositionFileName(checklistItem, undefined, file),
+              });
+              uploadTask.snapshot.ref.getDownloadURL().then((downloadURL: string) => {
+                resolve({ url: downloadURL, name: file.name, storedName: storedFileName });
+              });
+            },
+          );
+        });
+      };
+
+      const requests = files.map((file: File) => {
+        return uploadFile(file).then(storedItem => {
+          return storedItem;
+        });
+      });
+
+      return Promise.all(requests);
+    },
+    [storageBasePath, checklistItem, enqueueSnackbar],
+  );
+
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      saveFiles(acceptedFiles)
+        .then((documents: StoredDocument[]) => {
+          console.log(documents, 'DOCUMENTS');
+          const values = documents.map(item => {
+            return {
+              uploadedBy: getActivityLogUserData(),
+              uploadedAt: new Date(),
+              name: item.name,
+              url: item.url,
+              storedName: item.storedName,
+            } as ChecklistItemValueDocument;
+          });
+          storeActivity(() => checklistItemFileAddedHandler(values));
+        })
+        .catch(err => {
+          console.error(`Error while storing files ${JSON.stringify(checklistItem, null, 2)}`, err);
+        });
+    },
+    [checklistItem, saveFiles, storeActivity, checklistItemFileAddedHandler, getActivityLogUserData],
+  );
+
+  const { getRootProps, getInputProps, open, isDragActive } = useDropzone({
+    onDrop: (acceptedFiles: File[]) => onDrop(acceptedFiles),
+    noClick: true,
+  });
+
   return (
     <Box
       id={'checklistItemRow_' + checklistItem.id}
@@ -177,7 +303,15 @@ const BookingRequestChecklistRow = ({ bookingRequest, checklistItem, isAdmin }: 
       my={1}
       flexDirection={isAdmin && checklistItem.valuesAdmin?.length === 0 ? 'row' : 'column'}
     >
-      <Box className={classes.root} display="flex" flexDirection="column" id={checklistItem.id} flex={1}>
+      <Box
+        className={isDragActive ? classes.dropZone : classes.root}
+        display="flex"
+        flexDirection="column"
+        id={checklistItem.id}
+        flex={1}
+        {...getRootProps()}
+      >
+        <input {...getInputProps()} />
         <Box display="flex" flexDirection="row">
           <Box flexDirection="row" alignContent="center">
             <a id={checklistItem.id} />
