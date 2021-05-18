a4fdffb53a6b83c6984f61b9c83ea15380d573e6
Marko Nenadović <marko.nenadovic@spicefactory.co>
2021-05-18 15:06:20 +0200

    Connect Book Now from schedule to Online booking form

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,12 +1,23 @@
 import React, { Fragment } from 'react';
 import RouteSearch from '../components/RouteSearch';
 import Meta from '../components/Meta';
+import { RouteSearchResult } from '../model/route-search/RouteSearchResults';
+import { useHistory } from 'react-router';
 
-const Routes: React.FC = () => (
-  <Fragment>
-    <Meta title="Schedule" />
-    <RouteSearch />
-  </Fragment>
-);
+const Routes: React.FC = () => {
+  const history = useHistory();
+
+  const handleBookNow = (schedule?: RouteSearchResult) => {
+    localStorage.setItem('schedule', JSON.stringify(schedule));
+    history.push('/online-booking');
+  };
+
+  return (
+    <Fragment>
+      <Meta title="Schedule" />
+      <RouteSearch handleBookNow={handleBookNow} />
+    </Fragment>
+  );
+};
 
 export default Routes;
