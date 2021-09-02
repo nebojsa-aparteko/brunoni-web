2d2905ac9039e9b110937bab0fef20233ba3354a
Marko Nenadović <marko.nenadovic@spicefactory.co>
2021-09-02 10:58:05 +0300

    Disable booking of requests with invalid currencies

--- src/components/bookingRequests/BookingRequestView.tsx
+++ src/components/bookingRequests/BookingRequestView.tsx
@@ -363,6 +363,17 @@ const BookingRequestView: React.FC<Props> = ({ bookingRequest }) => {
   const availableTags = useContext(Tags);
   const { isOpen, openModal, closeModal } = useModal();
   const [isComparisonOpen, setIsComparisonOpen] = useState<boolean>(false);
+  const [isBookNowAutomaticallyDisabled, setIsBookNowAutomaticallyDisabled] = useState<boolean>(true);
+  const [isBookNowDisabled, setIsBookNowDisabled] = useState<boolean>(
+    bookingRequest.statusCode >= BookingRequestStatusCode.CONFIRMED || isBookNowAutomaticallyDisabled,
+  );
+
+  useEffect(() => {
+    setIsBookNowDisabled(
+      bookingRequest.statusCode >= BookingRequestStatusCode.CONFIRMED || isBookNowAutomaticallyDisabled,
+    );
+  }, [bookingRequest.statusCode, isBookNowAutomaticallyDisabled]);
+
   const {
     isOpen: isOpenAssignmentModal,
     closeModal: closeAssignmentModal,
@@ -761,7 +772,10 @@ const BookingRequestView: React.FC<Props> = ({ bookingRequest }) => {
     <Grid container direction="row" spacing={2} justify="center" alignItems="flex-start" className={classes.body}>
       <Grid item md={7} xs={12}>
         <Page title={getBookingRequestTitle(bookingRequest)}>
-          <MissingFields bookingRequest={bookingRequest} />
+          <MissingFields
+            bookingRequest={bookingRequest}
+            setIsBookNowButtonDisabled={value => setIsBookNowAutomaticallyDisabled(value)}
+          />
           {activities && activities?.length > 0 && (
             <Box my={2}>
               <Box displayPrint="none">
@@ -836,10 +850,7 @@ const BookingRequestView: React.FC<Props> = ({ bookingRequest }) => {
               <Box flex="1" />
               <Box display={'flex'} alignItems={'center'}>
                 {!editing && isDashboardUser(userRecord) && (
-                  <BookNowButton
-                    bookNow={bookNow}
-                    disabled={bookingRequest.statusCode >= BookingRequestStatusCode.CONFIRMED}
-                  />
+                  <BookNowButton bookNow={bookNow} disabled={isBookNowDisabled} />
                 )}
                 <Box className={classes.actions} displayPrint="none">
                   <EditButton
