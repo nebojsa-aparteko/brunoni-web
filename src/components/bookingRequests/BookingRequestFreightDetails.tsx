3641522b7f45a4728ed082b3760e4079fcea3a22
Marko Nenadović <marko.nenadovic@spicefactory.co>
2021-09-24 14:23:18 +0200

    Change the charge code input on intenal 2 tab in freight details to only accept predefined values

--- src/components/bookingRequests/BookingRequestFreightDetails.tsx
+++ src/components/bookingRequests/BookingRequestFreightDetails.tsx
@@ -198,7 +198,6 @@ const BookingRequestFreightDetailsRow: React.FC<RowProps> = ({
   const [quantity, setQuantity] = useState<number | undefined>(freightDetail.Anz || 0);
   const [unitValue, setUnitValue] = useState<number | undefined>(freightDetail.UnitValue);
   const [costUnit, setCostUnit] = useState<string | undefined>(freightDetail.Unit);
-  const [chargeCodeText, setChargeCodeText] = useState<string | undefined>(freightDetail.Unit);
   const [containerTypeNames, setContainerTypeNames] = useState(
     containerTypes?.map(containerType => containerType.name),
   );
@@ -218,13 +217,7 @@ const BookingRequestFreightDetailsRow: React.FC<RowProps> = ({
     setQuantity(freightDetail.Anz);
     setUnitValue(freightDetail.UnitValue);
     setCostUnit(freightDetail.Unit);
-    setChargeCodeText(freightDetail.Txt);
   }, [freightDetail]);
-
-  // const handleChangeChargeCode = (code: ChargeCode | undefined) => {
-  //   handleChangeFreightDetails(code?.text, 'Txt');
-  //   handleChangeFreightDetails(code?.chargeCodeId, 'ChargeID');
-  // };
 
   const handleChangeFreightDetails = (value: any | undefined, fieldName: string) => {
     bookingRequest &&
@@ -271,24 +264,12 @@ const BookingRequestFreightDetailsRow: React.FC<RowProps> = ({
             )}
             <TableCell component="th" scope="row">
               {editing && isDashboardUser(userRecord) ? (
-                selectedTab !== 2 ? (
-                  <ChargeCodeInput
-                    chargeCodeText={freightDetail.Txt}
-                    group={freightDetail.Group}
-                    handleChange={code => handleChangeFreightDetails(code?.text, 'Txt')}
-                    margin="dense"
-                  />
-                ) : (
-                  <TextField
-                    label=""
-                    margin="dense"
-                    variant="outlined"
-                    fullWidth
-                    value={chargeCodeText || ''}
-                    onChange={event => setChargeCodeText(event.target.value)}
-                    onBlur={event => handleChangeFreightDetails(event.target.value, 'Txt')}
-                  />
-                )
+                <ChargeCodeInput
+                  chargeCodeText={freightDetail.Txt}
+                  group={freightDetail.Group}
+                  handleChange={code => handleChangeFreightDetails(code?.text, 'Txt')}
+                  margin="dense"
+                />
               ) : (
                 freightDetail.Txt
               )}
