1472a33d82f090a040da4c4db9a6d9f98db84fbb
filipantic <filip.antic@spicefactory.co>
2021-10-15 14:59:10 +0200

    Add land transport into navigation for admins

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,7 @@ import EquipmentControlPage from './pages/EquipmentControlPage';
 import OnlineBookingPage from './pages/OnlineBookingPage';
 import BookingRequestContainer from './components/bookingRequests/BookingRequestContainer';
 import BookingRequestsFilterProvider from './providers/BookingRequestsFilterProvider';
+import LandTransportPage from './pages/LandTransportPage';
 
 const anonymousRoutes = (
   <Switch>
@@ -81,7 +82,7 @@ const adminRoutes = (
     <Route path="/charges" component={AdminSideCharges} />
     <Route exact path="/vessel" component={VesselWithVoyagePage} />
     {/*TODO uncomment this once it's ready*/}
-    {/*<Route exact path="/land-transport" component={LandTransportPage} />*/}
+    <Route exact path="/land-transport" component={LandTransportPage} />
     <Route exact path="/loadList" component={LoadListPage} />
     <Route exact path="/equipment-control" component={EquipmentControlPage} />
     <Route exact path="/my-day" component={MyDayPage} />
