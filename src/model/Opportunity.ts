13d28b74fb0209ab08c449d2fc4ec1e39a23b08a
Uros Dimitrijevic <urosdim@icloud.com>
2025-07-09 14:33:50 +0200

    creating model for opportunities

---
+++ src/model/Opportunity.ts
@@ -0,0 +1,24 @@
+import Client from './Client';
+import { OpportunityCommodityGroup } from './OpportunityCommodityGroup';
+import { OpportunityEquipmentGroup } from './OpportunityEquipmentGroup';
+import { OpportunityPlacesGroup } from './OpportunityPlacesGroup';
+import { OpportunityPortsGroup } from './OpportunityPortsGroup';
+import { OpportunityTag } from './OpportunityTag';
+import UserRecord from './UserRecord';
+
+export interface Opportunity {
+  id: string;
+  sleasRep: UserRecord;
+  bookingParty: Client;
+  shipper?: string;
+  cosignee?: string;
+  kindOfQuote: string;
+  placeOfReceiptGroupId?: OpportunityPlacesGroup;
+  portOfLoadingGroupId?: OpportunityPortsGroup;
+  portOfDischargeGroupId?: OpportunityPortsGroup;
+  placeOfDeliveryGroupId?: OpportunityPlacesGroup;
+  commodityGroupIds?: OpportunityCommodityGroup[];
+  equipmentGroupIds?: OpportunityEquipmentGroup[];
+  tag?: OpportunityTag[];
+  capacityTEU: number;
+}
