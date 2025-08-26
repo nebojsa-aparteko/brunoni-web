da6a0a53ecd88b4a03f84abe2128c61bad22103d
Milos Zikic <milos@spicefactory.co>
2025-08-26 09:59:56 -0400

    Do not exhaustive-deps

--- eslint.config.js
+++ eslint.config.js
@@ -79,7 +79,7 @@ module.exports = [
 
       // Hooks best practices
       'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/exhaustive-deps': 'error',
 
       // TypeScript ergonomics
       '@typescript-eslint/explicit-module-boundary-types': 'off',
